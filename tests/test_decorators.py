01fc1bf58bf51cb32e81fb855365ad0dfe84b25c
eml <1065873330@qq.com>
2023-08-24 15:49:18 +0800

    support async func

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,7 +1,6 @@
 from yast import AsgiApp, Response, TestClient
 
-def test_asgi_app():
-    
+def test_asgi_sync():
     @AsgiApp
     def app(request):
         return Response('hello, response', media_type='text/plain')
@@ -11,3 +10,26 @@ def test_asgi_app():
     res = client.get('/')
     assert res.status_code == 200
     assert res.text == 'hello, response'
+
+def test_asgi_async():
+    @AsgiApp
+    async def app(request):
+        return Response('hello, response', media_type='text/plain')
+    
+    client = TestClient(app)
+
+    res = client.get('/')
+    assert res.status_code == 200
+    assert res.text == 'hello, response'
+
+def test_asgi_async_body():
+    @AsgiApp
+    async def app(request):
+        body = await request.body()
+        return Response('hello, response:' + body.decode(), media_type='text/plain')
+    
+    client = TestClient(app)
+
+    res = client.post('/', data='111')
+    assert res.status_code == 200
+    assert res.text == 'hello, response:111'
--- yast/decorators.py
+++ yast/decorators.py
@@ -1,14 +1,18 @@
 from .request import Request
 from .response import Response
 from .types import ASGIInstance, Recevie, Send, Scope
-from asyncio.coroutines import iscoroutine
+from asyncio.coroutines import iscoroutinefunction
 
 def AsgiApp(func):
+    is_coroutine = iscoroutinefunction(func)
+
     def app(scope: Scope) -> ASGIInstance:
-        req = Request(scope)
         async def awaitable(recv: Recevie, send: Send) -> None:
-            req.set_recevie_channel(recv)
-            res = func(req)
+            req = Request(scope, recv)
+            if is_coroutine:
+                res = await func(req)
+            else:    
+                res = func(req)
             
             await res(recv, send)
 
