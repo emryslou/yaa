74ef3bca3d98ff9d4effb80a0210e44edf329311
eml <1065873330@qq.com>
2023-09-05 16:29:32 +0800

    middleware trustedhost: wildcards for domain

--- tests/middlewares/test_trustedhost.py
+++ tests/middlewares/test_trustedhost.py
@@ -1,20 +1,21 @@
 import pytest
 
 from yast.applications import Yast
+from yast.middlewares import TrustedHostMiddleware
 from yast.responses import PlainTextResponse
 from yast.testclient import TestClient
 
 
 def test_trustedhost():
-    from yast.middlewares import TrustedHostMiddleware
-
     app = Yast()
 
     @app.route('/')
     async def home(_):
         return PlainTextResponse('OK')
     
-    app.add_middleware(TrustedHostMiddleware, allowed_hosts=['aa.com', 'bb.com'])
+    app.add_middleware(
+            TrustedHostMiddleware, allowed_hosts=['aa.com', 'bb.com']
+        )
 
     client = TestClient(app, base_url='http://aa.com')
     res = client.get('/')
@@ -30,3 +31,38 @@ def test_trustedhost():
     res = client.get('/')
     assert res.status_code == 400
     assert res.text == 'Invalid host header'
+
+
+def test_wildcard():
+    app = Yast()
+
+    @app.route('/')
+    async def home(_):
+        return PlainTextResponse('OK')
+    
+    app.add_middleware(
+            TrustedHostMiddleware,
+            allowed_hosts=['aa.com', 'bb.com', '*.ff.com']
+        )
+
+    client = TestClient(app, base_url='http://aa.com')
+    res = client.get('/')
+    assert res.status_code == 200
+
+    client = TestClient(app, base_url='http://cc.aa.com')
+    res = client.get('/')
+    assert res.status_code == 400
+
+    client = TestClient(app, base_url='http://kk.ff.com')
+    res = client.get('/')
+    assert res.status_code == 200
+
+    client = TestClient(app, base_url='http://aa.ff.com')
+    res = client.get('/')
+    assert res.status_code == 200
+
+
+    client = TestClient(app, base_url='http://ff.aa.com')
+    res = client.get('/')
+    assert res.status_code == 400
+
--- yast/middlewares/trustedhost.py
+++ yast/middlewares/trustedhost.py
@@ -2,8 +2,16 @@ from yast.datastructures import Headers
 from yast.responses import Response, PlainTextResponse
 from yast.types import ASGIApp, Scope
 
+ENFORCE_DOMAIN_WILDCARD = (
+    "Domain wildcard patterns must be like '*.example.com'."
+)
+
 class TrustedHostMiddleware(object):
     def __init__(self, app: ASGIApp, allowed_hosts=['*']) -> None:
+        for pattern in allowed_hosts:
+            assert '*' not in pattern[1:], ENFORCE_DOMAIN_WILDCARD
+            if pattern.startswith('*') and pattern != '*':
+                assert pattern.startswith('*'), ENFORCE_DOMAIN_WILDCARD
         self.app = app
         self.allowed_hosts = allowed_hosts
         self.allow_any = '*' in self.allowed_hosts
@@ -11,8 +19,15 @@ class TrustedHostMiddleware(object):
     def __call__(self, scope: Scope) -> Response:
         if scope['type'] in ('http', 'websocket') and not self.allow_any:
             headers = Headers(scope=scope)
-            host = headers.get('host')
-            if host not in self.allowed_hosts:
+            host = headers.get('host', '').split(':')[0]
+            for pattern in self.allowed_hosts:
+                if (
+                        host == pattern or 
+                        pattern.startswith('*') and 
+                        host.endswith(pattern[1:])
+                    ):
+                    break
+            else:
                 return PlainTextResponse('Invalid host header', status_code=400)
 
         return self.app(scope) 
