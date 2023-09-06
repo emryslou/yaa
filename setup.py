4f035d5a67d6de518b359e420c41f0cbdd82778d
eml <1065873330@qq.com>
2023-09-06 14:43:37 +0800

    docs

--- setup.py
+++ setup.py
@@ -7,12 +7,51 @@ def get_version(package):
     init_py = open(os.path.join(package, '__init__.py')).read()
     return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
+def get_description(package):
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__description__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+def get_packages(packages):
+    return [
+        dirpath
+        for dirpath, _, _ in os.walk(packages)
+        if os.path.exists(os.path.join(dirpath, '__init__.py'))
+    ]
+
 setup(
     name="yast",
     version=get_version('yast'),
-    url='https://github.com/emryslou/yast/',
-    packages=['yast'],
+    description=get_description('yast'),
+    url='https://github.com/emryslou/yast',
+    author='emrys.lou',
+    author_email='1065873330@qq.com',
+    packages=get_packages('yast'),
     package_dir={
         "yast": "yast",
-    }
+    },
+    extras_requires = {
+        'full': [
+            'requests',
+            'aiofiles',
+            'ujson',
+            'python-multipart',
+            'graphql-core < 3',
+            'graphene',
+            'isort',
+            'itsdangerous',
+        ],
+        'test': [
+            'pytest',
+            'pytest-cov',
+            'pytest-timeout',
+            'black',
+        ],
+        'docs': [
+            'mkdocs',
+            'mkdocs-material',
+        ]
+    },
+    classifiers=[
+        'Programing LanguageLL Python :: 3.10.12',
+    ],
 )
