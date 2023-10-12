f860a6c0ba8ce0551ec2e032b613881d67db02ad
eml <1065873330@qq.com>
2023-10-12 15:30:38 +0800

    mypy

--- yaa/formparsers.py
+++ yaa/formparsers.py
@@ -40,7 +40,9 @@ def _user_safe_decode(src: bytes, codec: str) -> str:
 
 class FormParser(object):
     def __init__(
-        self, headers: Headers = None, stream: typing.AsyncGenerator[bytes, None] = None
+        self,
+        headers: typing.Optional[Headers] = None,
+        stream: typing.Optional[typing.AsyncGenerator[bytes, None]] = None,
     ) -> None:
         assert (
             multipart is not None
@@ -73,34 +75,41 @@ class FormParser(object):
 
         parser = multipart.QuerystringParser(callbacks)
         field_name, field_value = b"", b""
-        items = []
-        async for chunk in self.stream():
-            if chunk:
-                parser.write(chunk)
-            else:
-                parser.finalize()
-            messages = list(self.messages)
-            self.messages.clear()
-            for msg_type, msg_bytes in messages:
-                if msg_type == FormMessage.FIELD_START:
-                    field_name, field_value = b"", b""
-                elif msg_type == FormMessage.FIELD_NAME:
-                    field_name += msg_bytes
-                elif msg_type == FormMessage.FIELD_DATA:
-                    field_value += msg_bytes
-                elif msg_type == FormMessage.FIELD_END:
-                    name = unquote_plus(field_name.decode("utf-8"))
-                    value = unquote_plus(field_value.decode("utf-8"))
-                    items.append((name, value))
-                # elif msg_type == FormMessage.END:
-                #     pass
+        items: list = []
+
+        if callable(self.stream):
+            async for chunk in self.stream():
+                if chunk:
+                    parser.write(chunk)
+                else:
+                    parser.finalize()
+                messages = list(self.messages)
+                self.messages.clear()
+                for msg_type, msg_bytes in messages:
+                    if msg_type == FormMessage.FIELD_START:
+                        field_name, field_value = b"", b""
+                    elif msg_type == FormMessage.FIELD_NAME:
+                        field_name += msg_bytes
+                    elif msg_type == FormMessage.FIELD_DATA:
+                        field_value += msg_bytes
+                    elif msg_type == FormMessage.FIELD_END:
+                        name = unquote_plus(field_name.decode("utf-8"))
+                        value = unquote_plus(field_value.decode("utf-8"))
+                        items.append((name, value))
+                    # elif msg_type == FormMessage.END:
+                    #     pass
+                # end for
+            # end async for
+        # end if
 
         return FormData(items=items)
 
 
 class MultiPartParser(object):
     def __init__(
-        self, headers: Headers = None, stream: typing.AsyncGenerator[bytes, None] = None
+        self,
+        headers: typing.Optional[Headers] = None,
+        stream: typing.Optional[typing.AsyncGenerator[bytes, None]] = None,
     ) -> None:
         assert (
             multipart is not None
@@ -134,7 +143,7 @@ class MultiPartParser(object):
         self.messages.append((MultiPartMessage.END, b""))
 
     async def parse(self) -> FormData:
-        content_type, params = parse_options_header(self.headers["Content-Type"])
+        content_type, params = parse_options_header(self.headers["Content-Type"])  # type: ignore
         charset = params.get(b"charset", "utf-8")
         if isinstance(charset, bytes):
             charset = charset.decode("utf-8")
@@ -151,61 +160,63 @@ class MultiPartParser(object):
         data = b""
         _file = None
 
-        items = []
-        item_headers = []
+        items: list = []
+        item_headers: list = []
 
-        async for chunk in self.stream():
-            parser.write(chunk)
-            messages = list(self.messages)
-            self.messages.clear()
+        if callable(self.stream):
+            async for chunk in self.stream():
+                parser.write(chunk)
+                messages = list(self.messages)
+                self.messages.clear()
 
-            for msg_type, msg_bytes in messages:
-                if msg_type == MultiPartMessage.PART_BEGIN:
-                    content_disposition = None
-                    content_type = b""
-                    data = b""
-                    item_headers = []
-                elif msg_type == MultiPartMessage.HEADER_FIELD:
-                    header_field += msg_bytes
-                elif msg_type == MultiPartMessage.HEADER_VALUE:
-                    header_value += msg_bytes
-                elif msg_type == MultiPartMessage.HEADER_END:
-                    # raw_headers.append((header_field.lower(), header_value))
-                    field = header_field.lower()
-                    if field == b"content-disposition":
-                        content_disposition = header_value
-                    elif field == b"content-type":
-                        content_type = header_value
-                    item_headers.append((field, header_value))
-                    header_field, header_value = b"", b""
-                elif msg_type == MultiPartMessage.HEADERS_FINISHED:
-                    disposition, options = parse_options_header(content_disposition)
-                    field_name = _user_safe_decode(options[b"name"], charset)
+                for msg_type, msg_bytes in messages:
+                    if msg_type == MultiPartMessage.PART_BEGIN:
+                        content_disposition = None
+                        content_type = b""
+                        data = b""
+                        item_headers = []
+                    elif msg_type == MultiPartMessage.HEADER_FIELD:
+                        header_field += msg_bytes
+                    elif msg_type == MultiPartMessage.HEADER_VALUE:
+                        header_value += msg_bytes
+                    elif msg_type == MultiPartMessage.HEADER_END:
+                        # raw_headers.append((header_field.lower(), header_value))
+                        field = header_field.lower()
+                        if field == b"content-disposition":
+                            content_disposition = header_value
+                        elif field == b"content-type":
+                            content_type = header_value
+                        item_headers.append((field, header_value))
+                        header_field, header_value = b"", b""
+                    elif msg_type == MultiPartMessage.HEADERS_FINISHED:
+                        disposition, options = parse_options_header(content_disposition)
+                        field_name = _user_safe_decode(options[b"name"], charset)
 
-                    if b"filename" in options:
-                        filename = _user_safe_decode(options[b"filename"], charset)
-                        _file = UploadFile(
-                            filename=filename,
-                            content_type=content_type.decode("latin-1"),
-                            headers=Headers(raw=item_headers),
-                        )
-                    else:
-                        _file = None
+                        if b"filename" in options:
+                            filename = _user_safe_decode(options[b"filename"], charset)
+                            _file = UploadFile(
+                                filename=filename,
+                                content_type=content_type.decode("latin-1"),
+                                headers=Headers(raw=item_headers),
+                            )
+                        else:
+                            _file = None
 
-                elif msg_type == MultiPartMessage.PART_DATA:
-                    if _file is None:
-                        data += msg_bytes
-                    else:
-                        await _file.write(msg_bytes)
-                elif msg_type == MultiPartMessage.PART_END:
-                    if _file is None:
-                        items.append((field_name, _user_safe_decode(data, charset)))
-                    else:
-                        await _file.seek(0)
-                        items.append((field_name, _file))
-                # elif msg_type == MultiPartMessage.END:
-                #     pass
+                    elif msg_type == MultiPartMessage.PART_DATA:
+                        if _file is None:
+                            data += msg_bytes
+                        else:
+                            await _file.write(msg_bytes)
+                    elif msg_type == MultiPartMessage.PART_END:
+                        if _file is None:
+                            items.append((field_name, _user_safe_decode(data, charset)))
+                        else:
+                            await _file.seek(0)
+                            items.append((field_name, _file))
+                    # elif msg_type == MultiPartMessage.END:
+                    #     pass
+                # end for
             # end for
-        parser.finalize()
-
+            parser.finalize()
+        # end if
         return FormData(items=items)
