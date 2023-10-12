6d23e90e6bc2f15334c3d8f315182deae062d69e
eml <1065873330@qq.com>
2023-10-12 17:31:52 +0800

    mypy

--- yaa/plugins/logging/middlewares/trace.py
+++ yaa/plugins/logging/middlewares/trace.py
@@ -2,21 +2,21 @@ import logging
 
 from yaa.datastructures import MutableHeaders
 from yaa.middlewares.core import Middleware
-from yaa.types import ASGIApp, Message, Receive, Scope, Send
+from yaa.types import ASGI3App, Message, Receive, Scope, Send
 
 trace_log = logging.getLogger("yaa.trace")
 
 
 class TraceMiddleware(Middleware):
     def __init__(
-        self, app: ASGIApp, log_config: dict = {}, debug: bool = False
+        self, app: ASGI3App, log_config: dict = {}, debug: bool = False
     ) -> None:
         super().__init__(app)
         self.debug = debug
         if log_config:
             pass  # pragma: no cover
 
-    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:  # type: ignore
         import uuid
 
         _x_trace_header = "X-Trace-Id"
@@ -31,14 +31,14 @@ class TraceMiddleware(Middleware):
             async def sender(message: Message) -> None:
                 if message["type"] == "http.response.start":
                     headers = MutableHeaders(scope=message)
-                    headers.append(_x_trace_header, _x_trace_value)
+                    headers.append(_x_trace_header, _x_trace_value)  # type: ignore
                 await send(message)
 
         else:
-            sender = send  # pragma: no cover
+            sender = send  # type: ignore  # pragma: no cover
 
         if _x_trace_value is None:
             _x_trace_value = str(uuid.uuid4())
-        scope["trace_id"] = _x_trace_value
+        scope["trace_id"] = _x_trace_value  # type: ignore
 
         await self.app(scope, receive, sender)
