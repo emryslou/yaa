2badb8a6117c7b62b34763e5f144a9c99c1fb6d4
eml <1065873330@qq.com>
2023-10-13 08:23:42 +0800

    mypy

--- yaa/plugins/lifespan/__init__.py
+++ yaa/plugins/lifespan/__init__.py
@@ -15,13 +15,15 @@ def plugin_init(app: Yaa, config: dict = {}) -> None:
     context = config.get("context", None)
     lifespan = Lifespan(context=context, **handlers)
 
-    def on_event(event_type: EventType, cls: Yaa) -> None:
-        lifespan.on_event(str(event_type))
+    def on_event(event_type: EventType, cls: Yaa) -> typing.Callable:
+        return lifespan.on_event(str(event_type))
 
-    def add_event_handler(event_type: EventType, func: typing.Callable, cls: Yaa) -> None:
+    def add_event_handler(
+        event_type: EventType, func: typing.Callable, cls: Yaa
+    ) -> None:
         lifespan.add_event_handler(str(event_type), func)
 
     app.router.lifespan = lifespan
-    
+
     setattr(app, "on_event", functools.partial(on_event, cls=app))
     setattr(app, "add_event_handler", functools.partial(add_event_handler, cls=app))
